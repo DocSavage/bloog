66ecc54093b99cdcdde799877d900519e43075fb
Bill Katz <billkatz@gmail.com>
2008-07-16 11:17:59 -0700

    Clean out some early js and start YUI additions

--- static/js/toggle.js
+++
@@ -1,7 +0,0 @@
-function toggleDiv(divid){
-  if(document.getElementById(divid).style.display == 'none'){
-    document.getElementById(divid).style.display = 'block';
-  }else{
-    document.getElementById(divid).style.display = 'none';
-  }
-}
